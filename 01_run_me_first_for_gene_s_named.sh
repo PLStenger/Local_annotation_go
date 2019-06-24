78dd30badd41c85d3df8e3841f364260dfe7f46b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2019-06-24 14:40:03 +0200

    Create 01_run_me_first_for_gene_s_named.sh

---
+++ 01_run_me_first_for_gene_s_named.sh
@@ -0,0 +1,42 @@
+#! /bin/bash 
+
+rm 02_run_me_second.sh
+
+echo "#! /bin/bash " >> 02_run_me_second.sh
+
+cat transcipts_and_uniprot_list.txt | while read line 
+do 
+echo -e "$line" 
+awk '{print "echo XXXX"$1"XXXX >> "$1$2"_uniprot_file.txtYYYYwget -q -O - http://www.uniprot.org/uniprot/"$2".txt >> "$1$2"_uniprot_file.txt"}' | sed $'s/XXXX/\'/g' | sed 's/YYYY/\'$'\n/g' >> 02_run_me_second.sh
+done
+
+rm 03_run_me_third_prep.sh
+
+echo '#! /bin/bash' >> 03_run_me_third_prep.sh
+echo 'rm *_2.txt' >> 03_run_me_third_prep.sh
+echo 'rm *_3.txt' >> 03_run_me_third_prep.sh
+echo 'rm ALL*' >> 03_run_me_third_prep.sh
+echo 'for FILE in $(ls *_uniprot_file.txt)' >> 03_run_me_third_prep.sh
+echo 'do' >> 03_run_me_third_prep.sh
+echo 'grep -E XXXXevm.model.scaffoldXXXX ${FILE##*/}  >> ${FILE##*/}_2.txt' >> 03_run_me_third_prep.sh
+echo 'grep -E XXXX^DEXXXX ${FILE##*/} | sed XXXXs/DE   //gXXXX | sed XXXXN;s/;\n/\; /gXXXX >> ${FILE##*/}_2.txt' >> 03_run_me_third_prep.sh
+echo 'tr "\n" "\t" < ${FILE##*/}_2.txt > ${FILE##*/}_3.txt' >> 03_run_me_third_prep.sh
+echo 'done' >> 03_run_me_third_prep.sh
+echo 'cat *_3.txt > ALL.txt' >> 03_run_me_third_prep.sh
+echo 'sed XXXXs/evm.model.scaffold/\XXXX$XXXX\nevm.model.scaffold/gXXXX ALL.txt > ALL_2.txt' >> 03_run_me_third_prep.sh
+echo 'awk XXXXF[$1]++ {$1=OFS}1XXXX ALL_2.txt > ALL_3.txt' >> 03_run_me_third_prep.sh
+echo 'sed XXXXs/  //gXXXX ALL_3.txt > ALL_4.txt' >> 03_run_me_third_prep.sh
+echo 'tr -d XXXX\nXXXX < ALL_4.txt > ALL_5.txt' >> 03_run_me_third_prep.sh
+echo 'sed XXXXs/evm.model.scaffold/\XXXX$XXXX\nevm.model.scaffold/gXXXX ALL_5.txt > ALL_6.txt' >> 03_run_me_third_prep.sh
+echo 'awk XXXX{$1=""; print $0}XXXX  ALL_6.txt > ALL_7.txt' >> 03_run_me_third_prep.sh
+echo 'awk XXXX{print $1}XXXX ALL_6.txt > ALL_10.txt' >> 03_run_me_third_prep.sh
+echo 'sed XXXXs/	//gXXXX ALL_7.txt > ALL_8.txt' >> 03_run_me_third_prep.sh
+echo 'paste ALL_10.txt ALL_8.txt > annotation_gene_named.txt' >> 03_run_me_third_prep.sh
+echo 'rm ALL*' >> 03_run_me_third_prep.sh
+echo 'rm *_2.txt' >> 03_run_me_third_prep.sh
+echo 'rm *_3.txt' >> 03_run_me_third_prep.sh
+echo 'rm *_uniprot_file.txt' >> 03_run_me_third_prep.sh
+
+sed $'s/XXXX/\'/g' 03_run_me_third_prep.sh > 03_run_me_third.sh
+
+rm 03_run_me_third_prep.sh
